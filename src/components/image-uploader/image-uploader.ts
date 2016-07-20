5de9e66530e9dece2b6cc1303ea22060e9cb10e7
Lukas Bos <LBS@afasgroep.nl>
2016-07-20 14:48:36 +0200

    camera now only loaded when modal is open

--- src/components/image-uploader/image-uploader.ts
+++ src/components/image-uploader/image-uploader.ts
@@ -13,13 +13,29 @@ let styles = <any>require('./image-uploader.css');
 export let createImageUploader = () => {
 
   let getUserMediaIsSupported = true;
-  let modalIsOpen = false; 
+  let modalIsOpen = false;
+
 
   let canvas: HTMLCanvasElement,
     context: any,
     video: HTMLVideoElement,
     videoObj: any,
-    errBack: any;
+    errBack: any,
+    n = <any>navigator,
+    mediaTrack: any; 
+
+    errBack = (error: any) => {
+      //getUserMediaIsSupported = false;
+      console.log(String(error)); 
+    };
+
+  let checkUserMediaSupport = () => {
+    if (!(n.getUserMedia || n.webkitGetUserMedia || n.mozGetUserMedia)) {
+      getUserMediaIsSupported = false;
+    }
+  }
+
+  checkUserMediaSupport();
 
   let handleAfterCreateCamera = () => {
     var w = <any>window;
@@ -29,33 +45,30 @@ export let createImageUploader = () => {
     context = canvas.getContext("2d");
     video = <HTMLVideoElement>document.getElementById("video");
     videoObj = { "video": true };
-    errBack = (error: any) => {
-      alert("Video capture error: " + error.code);
-    };
 
-    var n = <any>navigator;
-    
     // Put video listeners into place
     if (n.getUserMedia) { // Standard
       n.getUserMedia(videoObj, function (stream: any) {
         video.src = URL.createObjectURL(stream);
         video.play();
+        mediaTrack = stream.getTracks()[0];
       }, errBack);
     } else if (n.webkitGetUserMedia) { // WebKit-prefixed
       n.webkitGetUserMedia(videoObj, function (stream: any) {
         video.src = URL.createObjectURL(stream);
         video.play();
+        mediaTrack = stream.getTracks()[0];
       }, errBack);
     }
     else if (n.mozGetUserMedia) { // Firefox-prefixed
       n.mozGetUserMedia(videoObj, function (stream: any) {
         video.src = window.URL.createObjectURL(stream);
         video.play();
+        mediaTrack = stream.getTracks()[0];
       }, errBack);
-    } else {
-      getUserMediaIsSupported = false;
-    }
+    } 
   }
+
 
   let getPicture = (event: any) => {
     if (event.target.files.length == 1 &&
@@ -63,7 +76,7 @@ export let createImageUploader = () => {
 
       var image = new Image();
       image.src = URL.createObjectURL(event.target.files[0]);
-      
+
       image.onload = function () {
         context.drawImage(image, 0, 0, 320, 240);
       }
@@ -75,8 +88,11 @@ export let createImageUploader = () => {
     console.log('screenshot created!');
   }
 
-  let toggleModal = () => { 
+  let toggleModal = () => {
     modalIsOpen = !modalIsOpen;
+    if (!modalIsOpen) {
+      mediaTrack.stop();
+    }
   }
 
   let openModalButton = createButton({
@@ -84,33 +100,37 @@ export let createImageUploader = () => {
     primary: false
   }, { onClick: toggleModal });
 
-      let createScreenshotButton = createButton({ text: 'Create Snapshot', primary: false }, { onClick: createScreenShot }); 
+  let createScreenshotButton = createButton({ text: 'Create Snapshot', primary: false }, { onClick: createScreenShot });
 
   return {
     renderMaquette: () => {
 
-
       let modal = createModal({
-        isOpen: modalIsOpen, 
-        title: "Create a snapshot", 
+        isOpen: modalIsOpen,
+        title: "Create a snapshot",
         contents: [
-           createScreenshotButton,
-           createLiveCamera({},{ startCameraAfterCreate: handleAfterCreateCamera })
-          ]}, 
-        {toggleModal: toggleModal}); 
+          createScreenshotButton,
+          createLiveCamera({}, { startCameraAfterCreate: handleAfterCreateCamera })
+        ]
+      },
+        { toggleModal: toggleModal });
 
       return h('div', [
-        openModalButton.renderMaquette(),
-        getUserMediaIsSupported ? modal.renderMaquette() : undefined, 
+        getUserMediaIsSupported ? [
+          modal.renderMaquette(),
+          openModalButton.renderMaquette()
+        ]
+          : undefined,
 
-        h('input', { type: "file", 
-                     capture: 'camera', 
-                     accept: 'image/*', 
-                     id: 'takePictureField', 
-                     onchange: getPicture 
-                   }),
+        h('input', {
+          type: "file",
+          capture: 'camera',
+          accept: 'image/*',
+          id: 'takePictureField',
+          onchange: getPicture
+        }),
 
-        h('canvas', { id: "canvas", width: '320', height: '240'}),
+        h('canvas', { id: "canvas", width: '320', height: '240' }),
       ]);
     }
   }
