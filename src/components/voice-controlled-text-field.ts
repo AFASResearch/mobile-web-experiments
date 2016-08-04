bac7afbec697ac84143dd4e7e8b7499d71d8d270
Lukas Bos <mrlukasbos@gmail.com>
2016-08-04 16:46:13 +0200

    improved functions and formatting of voice input component

--- src/components/voice-controlled-text-field.ts
+++ src/components/voice-controlled-text-field.ts
@@ -16,71 +16,70 @@ export let createVoiceControlledTextField = (config: VoiceControlledTextFieldCon
   let {label, prefilled, projector} = config;
   let {getValue, setValue} = bindings;
 
-
-
-
   let recognizedSpeech = '';
   let isListening = false;
   let startStopButtonText = 'start listening';
 
+  let stopListening = () => {
+    recognition.stop();
+    startStopButtonText = 'start listening';
+    isListening = false;
+    projector.scheduleRender();
+  }
+
+  let startListening = () => { config
+    recognition.start();
+    startStopButtonText = 'stop listening';
+    isListening = true;
+    projector.scheduleRender();
+  }
+
   if (!('webkitSpeechRecognition' in window)) {
-      //Speech API not supported here…
+    //Speech API not supported here…
+    console.log('speech api is not supported :(')
   } else { //Let’s do some cool stuff :)
-      var recognition = new webkitSpeechRecognition(); //That is the object that will manage our whole recognition process.
-      recognition.continuous = true;   //Suitable for dictation.
-      recognition.interimResults = true;  //If we want to start receiving results even if they are not final.
-      //Define some more additional parameters for the recognition:
-      recognition.lang = "nl_NL";
-      recognition.maxAlternatives = 1; //Since from our experience, the highest result is really the best...
+    var recognition = new webkitSpeechRecognition(); //That is the object that will manage our whole recognition process.
+    recognition.continuous = true;   //Suitable for dictation.
+    recognition.interimResults = true;  //If we want to start receiving results even if they are not final.
+    //Define some more additional parameters for the recognition:
+    recognition.lang = "nl_NL";
+    recognition.maxAlternatives = 1; //Since from our experience, the highest result is really the best...
 
-      recognition.onstart = function() {
-          //Listening (capturing voice from audio input) started.
-          //This is a good place to give the user visual feedback about that (i.e. flash a red light, etc.)
-          console.log(' started listening ')
-          isListening = true;
-          projector.scheduleRender();
+    recognition.onstart = function() {
+      // here we could do things when the recognition has started. i.e. animations.
+    };
 
-      };
+    recognition.onend = function() {
+      stopListening();
+    };
 
-      recognition.onend = function() {
-          console.log(' stopped listening ')
-          isListening = false;
-          startStopButtonText = 'start listening';
+    recognition.onresult = function(event: any) { //the event holds the results
+      if (typeof(event.results) === 'undefined') { //Something is wrong…
+        recognition.stop();
+        startStopButtonText = 'start listening';
+        return;
+      }
 
-          projector.scheduleRender();
-
-      };
-
-      recognition.onresult = function(event: any) { //the event holds the results
-          //Yay – we have results! Let’s check if they are defined and if final or not:
-          if (typeof(event.results) === 'undefined') { //Something is wrong…
-              recognition.stop();
-              startStopButtonText = 'start listening';
-              return;
-          }
-
-          for (var i = event.resultIndex; i < event.results.length; ++i) {
-              if (event.results[i].isFinal) { // Final results
-                  console.log("final results: " + event.results[i][0].transcript);   //Of course – here is the place to do useful things with the results.
-                  recognizedSpeech = event.results[i][0].transcript;
-              } else {   //i.e. interim...
-                  console.log("interim results: " + event.results[i][0].transcript);  //You can use these results to give the user near real time experience.
-                  recognizedSpeech = event.results[i][0].transcript;
-              }
-          } //end for loop
-          projector.scheduleRender();
-      };
+      for (var i = event.resultIndex; i < event.results.length; ++i) {
+        if (event.results[i].isFinal) {
+          // Final results; here is the place to do useful things with the results.
+          console.log("final results: " + event.results[i][0].transcript);
+          recognizedSpeech = event.results[i][0].transcript;
+        } else {
+          // i.e. interim. You can use these results to give the user near real time experience.
+          console.log("interim results: " + event.results[i][0].transcript);
+          recognizedSpeech = event.results[i][0].transcript;
+        }
+      }
+      projector.scheduleRender();
+    };
   }
 
   let startOrStopListening = () => {
     if (isListening) {
-      recognition.stop();
-      startStopButtonText = 'start listening';
-      console.log('I stop listening now...');
+      stopListening();
     } else {
-      recognition.start();
-      startStopButtonText = 'stop listening';
-      console.log('I start listening now...');
+      startListening();
     }
     isListening = !isListening;
   }
@@ -102,5 +101,5 @@ export let createVoiceControlledTextField = (config: VoiceControlledTextFieldCon
       ]);
     }
   };
-return textField;
+  return textField;
 };
